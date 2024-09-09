629713fe12c4ac3d73f75db005ed90b83779e158
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-09 22:54:17 +0700

    Chapter 53: Object Oriented Programming In C#

---
+++ CSharpNotesForProfessionals/Chapter 53 Object Oriented Programming/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Section 53.1: Classes:");
+int i = 100;
+void getMyValue()
+{
+    Console.WriteLine(i);//Will print number 100 in output
+}
