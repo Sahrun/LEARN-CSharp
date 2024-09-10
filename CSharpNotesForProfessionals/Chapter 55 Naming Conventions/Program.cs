5319ce9327d9b0a8162879caa3c47c33d6ae939a
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-10 22:56:01 +0700

    Chapter 55: Naming Conventions

---
+++ CSharpNotesForProfessionals/Chapter 55 Naming Conventions/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Section 55.1: Capitalization conventions");
+
+Console.WriteLine("Section 55.2: Enums");
+
+Console.WriteLine("Section 55.3: Interfaces");
+
+Console.WriteLine("Section 55.4: Exceptions");
+
+Console.WriteLine("Section 55.5: Private fields");
+
+
+Console.WriteLine("Section 55.6: Namespaces");
