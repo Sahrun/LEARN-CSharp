8d162e72d81c4d87ce652ce82139594032b69b63
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-11 08:53:55 +0700

    Chapter 57: Comments and regions

---
+++ CSharpNotesForProfessionals/Chapter 57 Comments and regions/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Chapter 57: Comments and regions");
+Console.WriteLine("Section 57.1: Comments");
+Console.WriteLine("Section 57.2: Regions");
+Console.WriteLine("Section 57.3: Documentation comments");
