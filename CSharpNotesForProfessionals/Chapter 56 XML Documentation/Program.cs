2e1b6f6e2b8c555b0eaedde760431dacbaf220dc
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-11 08:19:17 +0700

    Chapter 56: XML Documentation Comments

---
+++ CSharpNotesForProfessionals/Chapter 56 XML Documentation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Chapter 56: XML Documentation Comments");
+Console.WriteLine("Section 56.1: Simple method annotation");
+Console.WriteLine("Section 56.2: Generating XML from documentation comments");
+Console.WriteLine("Section 56.3: Method documentation comment with param and returns elements");
+Console.WriteLine("Section 56.4: Interface and class documentation comments");
+Console.WriteLine("Section 56.5: Referencing another class in documentation");
