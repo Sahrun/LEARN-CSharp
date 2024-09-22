3aeb7acc6783b020a84834fc031470168297c7a9
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-22 22:57:40 +0700

    Chapter 61: Using Directive

---
+++ CSharpNotesForProfessionals/Chapter 61 Using Directive/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+Console.WriteLine("Chapter 61: Using Directive");
+
+Console.WriteLine("Section 61.1: Associate an Alias to Resolve Conflicts");
+
+Console.WriteLine("Section 61.2: Using alias directives");
+
+Console.WriteLine("Section 61.3: Access Static Members of a Class");
+
+Console.WriteLine("Section 61.4: Basic Usage");
+
+Console.WriteLine("Section 61.5: Reference a Namespace");
+
+Console.WriteLine("Section 61.6: Associate an Alias with a Namespace");
