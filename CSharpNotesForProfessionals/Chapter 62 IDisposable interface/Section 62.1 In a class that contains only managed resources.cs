4072b616a855ced241c65db3807608e00996568d
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-24 23:24:40 +0700

    Chapter 62: IDisposable interface

---
+++ CSharpNotesForProfessionals/Chapter 62 IDisposable interface/Section 62.1 In a class that contains only managed resources.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_62_IDisposable_interface_Section_62
+{
+    internal class ObjectWithManagedResourcesOnly : IDisposable
+    {
+        private SqlConnection sqlConnection = new SqlConnection();
+
+        public void Dispose()
+        {
+            sqlConnection.Dispose();
+        }
+    }
+}
