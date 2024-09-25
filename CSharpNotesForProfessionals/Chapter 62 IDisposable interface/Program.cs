4aeb6a6040a9ce14f84d8c41f701ae16947b6ed1
Sahrun <sahrun.raja.tega@gmail.com>
2024-09-25 11:13:23 +0700

    Section 62.1: In a class that contains only managed resources

--- CSharpNotesForProfessionals/Chapter 62 IDisposable interface/Program.cs
+++ CSharpNotesForProfessionals/Chapter 62 IDisposable interface/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Section 62.1: In a class that contains only managed resources");
 Console.WriteLine("Section 62.2: In a class with managed and unmanaged resources");
 Console.WriteLine("Section 62.3: IDisposable, Dispose");
-Console.WriteLine("Section 62.4: using keyword\r\n");
-Console.WriteLine("Section 62.5: In an inherited class with managed resources\r\n");
+Console.WriteLine("Section 62.4: using keyword");
+Console.WriteLine("Section 62.5: In an inherited class with managed resources");
